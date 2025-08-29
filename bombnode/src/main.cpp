c82fe5d1e1fe60f4dfb948813f2ca990e328a20b
gurraba <gurraba@gmail.com>
2025-08-29 22:46:16 +0200

    very cool
    all modules implemented

--- bombnode/src/main.cpp
+++ bombnode/src/main.cpp
@@ -1,36 +1,87 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <OSCMessage.h>
-
 #include <WiFiUdp.h>
 
+#include "FastLED.h"
 
 //specifics for lcd display
 #include <Wire.h>
 #include <Adafruit_RGBLCDShield.h>
 #include <utility/Adafruit_MCP23017.h>
 
-Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
-#define WHITE 0x7
+// for the 14 segment display
+#include <Adafruit_GFX.h>
+#include <Adafruit_LEDBackpack.h>
 
-uint8_t buttons = lcd.readButtons();
+#ifndef _BV
+  #define _BV(bit) (1<<(bit))
+#endif
 
-uint8_t lastButtons = 0;
+Adafruit_LEDBackpack segDisplay = Adafruit_LEDBackpack();
+
+#define ALPHANUM_SEG_A 0b0000000000000001 ///&lt; Alphanumeric segment A
+#define ALPHANUM_SEG_B 0b0000000000000010 ///&lt; Alphanumeric segment B
+#define ALPHANUM_SEG_C 0b0000000000000100 ///&lt; Alphanumeric segment C
+#define ALPHANUM_SEG_D 0b0000000000001000 ///&lt; Alphanumeric segment D
+#define ALPHANUM_SEG_E 0b0000000000010000 ///&lt; Alphanumeric segment E
+#define ALPHANUM_SEG_F 0b0000000000100000 ///&lt; Alphanumeric segment F
+#define ALPHANUM_SEG_G1 0b0000000001000000 ///&lt; Alphanumeric segment G1
+#define ALPHANUM_SEG_G2 0b0000000010000000 ///&lt; Alphanumeric segment G2
+#define ALPHANUM_SEG_H 0b0000000100000000 ///&lt; Alphanumeric segment H
+#define ALPHANUM_SEG_J 0b0000001000000000 ///&lt; Alphanumeric segment J
+#define ALPHANUM_SEG_K 0b0000010000000000 ///&lt; Alphanumeric segment K
+#define ALPHANUM_SEG_L 0b0000100000000000 ///&lt; Alphanumeric segment L
+#define ALPHANUM_SEG_M 0b0001000000000000 ///&lt; Alphanumeric segment M
+#define ALPHANUM_SEG_N 0b0010000000000000 ///&lt; Alphanumeric segment N
+#define ALPHANUM_SEG_DP 0b0100000000000000 ///&lt; Alphanumeric segment DP
 
 
-// put function declarations here:
 
-void sendOscCommand(const char *address);
-void display(String msg, int x, int y, bool clear);
+int numbers[10] = {
+    // 0
+    ALPHANUM_SEG_A | ALPHANUM_SEG_B | ALPHANUM_SEG_C | ALPHANUM_SEG_D |
+    ALPHANUM_SEG_E | ALPHANUM_SEG_F,
+    
+    // 1
+    ALPHANUM_SEG_B | ALPHANUM_SEG_C,
+    
+    // 2
+    ALPHANUM_SEG_A | ALPHANUM_SEG_B | ALPHANUM_SEG_D | ALPHANUM_SEG_E |
+    ALPHANUM_SEG_G1 | ALPHANUM_SEG_G2,
+    
+    // 3
+    ALPHANUM_SEG_A | ALPHANUM_SEG_B | ALPHANUM_SEG_C | ALPHANUM_SEG_D |
+    ALPHANUM_SEG_G1 | ALPHANUM_SEG_G2,
+    
+    // 4
+    ALPHANUM_SEG_B | ALPHANUM_SEG_C | ALPHANUM_SEG_F | ALPHANUM_SEG_G1 |
+    ALPHANUM_SEG_G2,
+    
+    // 5
+    ALPHANUM_SEG_A | ALPHANUM_SEG_C | ALPHANUM_SEG_D | ALPHANUM_SEG_F |
+    ALPHANUM_SEG_G1 | ALPHANUM_SEG_G2,
+    
+    // 6
+    ALPHANUM_SEG_A | ALPHANUM_SEG_C | ALPHANUM_SEG_D | ALPHANUM_SEG_E |
+    ALPHANUM_SEG_F | ALPHANUM_SEG_G1 | ALPHANUM_SEG_G2,
+    
+    // 7
+    ALPHANUM_SEG_A | ALPHANUM_SEG_B | ALPHANUM_SEG_C,
+    
+    // 8
+    ALPHANUM_SEG_A | ALPHANUM_SEG_B | ALPHANUM_SEG_C | ALPHANUM_SEG_D |
+    ALPHANUM_SEG_E | ALPHANUM_SEG_F | ALPHANUM_SEG_G1 | ALPHANUM_SEG_G2,
+    
+    // 9
+    ALPHANUM_SEG_A | ALPHANUM_SEG_B | ALPHANUM_SEG_C | ALPHANUM_SEG_D |
+    ALPHANUM_SEG_F | ALPHANUM_SEG_G1 | ALPHANUM_SEG_G2
+};
 
-void createSweCharacters(); 
-String replaceSpecialChars(String str);
-void displayOscMessage(OSCMessage &msg);
 
 
-
+// WIFI LOGIC STUFF, YOU MAY NEED TO CHANGE THIS
 const int BOMBID = 1337;
-
 const char* ssid = "bombnet";
 const char* password = "sprangnollan";
 
@@ -39,34 +90,158 @@ IPAddress localIp;
 unsigned int recvPort = 5000;
 unsigned int targetPort = 4000;
 
-IPAddress targetIp = IPAddress(192, 168, 1, 223);
+IPAddress targetIp = IPAddress(192, 168, 1, 223); // does not matter
 
 WiFiUDP udp;
 
 char* recvbuf = new char[1024];
 
+//---------------------------------------------------------
+struct debouncedInput {
+  int pin;
+  bool state;         // debounced state
+  bool lastState = state;     // last stable state
+  bool lastReading;   // last raw reading
+  unsigned long lastDebounceTime;
+};
+
+void initInput(debouncedInput &input, int pin, bool startingState) {
+  input.pin = pin;
+  pinMode(pin, INPUT);
+  input.state = startingState;
+  input.lastReading = startingState;
+  input.lastDebounceTime = millis();
+}
+
+void updateInput(debouncedInput &input, unsigned long debounceDelay = 100) {
+  bool reading = digitalRead(input.pin);
+
+  if (reading != input.lastReading) {
+    input.lastDebounceTime = millis();  // reset timer on change
+
+  }
+
+  if ((millis() - input.lastDebounceTime) > debounceDelay) {
+    // update state if stable long enough
+    input.lastState = input.state;
+    input.state = reading;
+  }
+
+  input.lastReading = reading;
+}
 
 
+
+// LCD display
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
+#define WHITE 0x7
+#define GREEN 0x3
+uint8_t buttons = lcd.readButtons();
+uint8_t lastButtons = 0;
+bool allWiresCut = false;
+
+// wires
+const int WIRE_PINS[5] = {12, 14, 27, 26, 25};
+bool wireStates[5] = {true, true, true, true, true};
+debouncedInput wireInputs[5];
+
+
+
+// LED lights
+#define LED_PIN 16
+#define NUM_LEDS 8
+CRGB leds[NUM_LEDS];
+int logicalLEDNumberToPhysicalPosition[NUM_LEDS] = {6, 7, 0, 1, 5, 4, 3, 2}; // mapping from logical led number to physical led index
+
+
+void setupLEDs() {
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
+  FastLED.setBrightness(50);
+}
+
+
+// "DO NOT PRESS" BUTTON LOGIC
+debouncedInput doNotPressButton;
+
+
+#define BUTTON_PIN 13 // Change this to the actual pin number
+bool buttonPressed = false;
+bool lastButtonState = false;
+//interrupt
+
+
+
+
+
+// put function declarations here:
+
+void sendOscCommand(const char *address);
+void display(String msg, int x, int y, bool clear);
+void createSweCharacters(); 
+String replaceSpecialChars(String str);
+void handleOSCMessage(OSCMessage &msg);
+void handleWordMaze(OSCMessage &msg);
+void LEDmanager(int LEDnNumber, CRGB color);
+void handleLED(OSCMessage &msg);
+void handleTimer(OSCMessage &msg);
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("Hello, world!");
-  Wire.begin(22, 19);
+
+  setupLEDs();
+  //light up all the leds in red
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[logicalLEDNumberToPhysicalPosition[i]] = CRGB::Red;
+    delay(100);
+    FastLED.show();
+  }
+  //turn off all LEDs
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[logicalLEDNumberToPhysicalPosition[i]] = CRGB::Black;
+    delay(100);
+    FastLED.show();
+  }
+
+
+  // Initialize I2C for LCD
+  Wire.begin(18, 23);
   Serial.println(SDA);
   Serial.println(SCL);
   lcd.begin(16, 2);
 
-  createSweCharacters();
+  createSweCharacters(); // custom chars for lcd
 
-  lcd.setCursor(0, 0);
+  segDisplay.begin(0x70);
+  segDisplay.setBrightness(10);
+  // segDisplay.displaybuffer[0] = numbers[0];
+  // segDisplay.writeDisplay();
+  //write all numbers 1-10 on the display
+  for (int i = 0; i < 10; i++) {
+    segDisplay.displaybuffer[0] = numbers[i];
+    segDisplay.displaybuffer[1] = numbers[i];
+    segDisplay.displaybuffer[2] = numbers[i];
+    segDisplay.displaybuffer[3] = numbers[i];
+    segDisplay.writeDisplay();
+    delay(100);
+  }
+  segDisplay.clear();
+
+  lcd.setCursor(0, 0); // for style only
   lcd.print("SLAVSOFT");
   lcd.setCursor(0, 1);
   lcd.print("INDUSTRIES");
-  lcd.setBacklight(HIGH);
+  lcd.setBacklight(GREEN);
 
-  delay(500);
+  for (int i = 0; i < 5; i++) {
+    initInput(wireInputs[i], WIRE_PINS[i], true);
+  }
+  initInput(doNotPressButton, BUTTON_PIN, false);
+
+  delay(1000);
+
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -74,11 +249,9 @@ void setup() {
   Serial.println("\nConnecting");
   Serial.println(ssid);
 
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Connecting...");
-  lcd.setCursor(0, 1);
-  lcd.print(ssid);
+
+  display("Connecting...", 0, 0, true);
+  display(ssid, 0, 1, false);
 
   while(WiFi.status() != WL_CONNECTED){
       Serial.print(".");
@@ -158,7 +331,13 @@ void setup() {
 
 }
 
+
+
+
+
 void loop() {
+
+
 
   if (udp.parsePacket() > 0)
   {
@@ -171,7 +350,8 @@ void loop() {
     Serial.print(msgrecv.getAddress());
     Serial.println("'");
 
-    displayOscMessage(msgrecv);
+    handleOSCMessage(msgrecv);
+    
   }
 
 
@@ -190,12 +370,70 @@ void loop() {
   lastButtons = buttons;
 
 
+  //wire cutting logic, 
+  if(!allWiresCut){
+    for (int i = 0; i < 5; i++) {
 
-  
+      updateInput(wireInputs[i]);
+
+      if (wireInputs[i].state == LOW && wireStates[i] == true) {
+        udp.beginPacket(targetIp, targetPort);
+        OSCMessage msg("/wires/cut/"); // 
+        msg.add(i);
+        msg.send(udp);
+        udp.endPacket();
+        msg.empty();
+        wireStates[i] = false;
+        Serial.println("Cut wire " + String(i));
+      }
+      
+      //check if all wires are cut, set allwirescut to true
+      allWiresCut = true;
+      for (int j = 0; j < 5; j++) {
+        if (wireStates[j] == true) {
+          allWiresCut = false;
+          break;
+        }
+      }
+    }
+
+
+    //"DO NOT PRESS" button logic
+    updateInput(doNotPressButton, 10);
+    Serial.println("Button state: " + String(doNotPressButton.state) + " last state: " + String(doNotPressButton.lastState));
+    if (doNotPressButton.state == HIGH && lastButtonState == false) {
+      // Send OSC message
+      udp.beginPacket(targetIp, targetPort);
+      OSCMessage msg("/button");
+      msg.add(true);
+      msg.send(udp);
+      udp.endPacket();
+      msg.empty();
+
+      lastButtonState = true;
+
+    }
+    else if (doNotPressButton.state == LOW && lastButtonState == true) {
+      // Send OSC message
+      udp.beginPacket(targetIp, targetPort);
+      OSCMessage msg("/button");
+      msg.add(false);
+      msg.send(udp);
+      udp.endPacket();
+      msg.empty();
+
+      lastButtonState = false;
+    }
+
+
+  }
+
+
 
 
 
   // put your main code here, to run repeatedly:
+
 }
 
 
@@ -307,16 +545,75 @@ String replaceSpecialChars(String str) {
   return str;
 }
 
-void displayOscMessage(OSCMessage &msg) {   // this functions displays the words for the word maze
-  if (msg.fullMatch("/wordmaze/display") && msg.isString(0)) { // check if the message is for displaying the first word
+void handleOSCMessage(OSCMessage &msg) {
+  if (msg.fullMatch("/wordmaze/display")) {
+    handleWordMaze(msg);
+  } 
+  else if (msg.fullMatch("/button/lights")) {
+    handleLED(msg);
+  } 
+  // else if (msg.fullMatch("/button/light/off")) {
+  //   handleLED(msg, false);
+  // } 
+
+  else if (msg.fullMatch("/timer")){
+    handleTimer(msg);
+  }
+
+
+}
+
+void handleTimer (OSCMessage &msg) {
+  // we recieve four different ints, 10min 1 min 10 sec 1 sec, unpack and display them on 14 seg
+  for(int i = 0; i < 4; i++) {
+    if (msg.isInt(i)) {
+      int value = msg.getInt(i);
+      // Display the value on the 14-segment display
+      segDisplay.displaybuffer[i] = numbers[value];
+    }
+  }
+  // light up decimal point
+  segDisplay.displaybuffer[1] |= ALPHANUM_SEG_DP*(msg.getInt(3)%2);
+  segDisplay.writeDisplay();
+}
+
+void handleWordMaze(OSCMessage &msg) {   // this functions displays the words for the word maze
+  if (msg.isString(0)) { // check if the message is for displaying the first word
     char buf[64]; 
     msg.getString(0, buf, sizeof(buf)); 
     display(buf, 0, 0, true); //clear display and show first argument/message on first row
   }
-  if (msg.fullMatch("/wordmaze/display") && msg.isString(1)) { //is there a second argument? if so, print it on the second rw
+  if (msg.isString(1)) { //is there a second argument? if so, print it on the second rw
     char buf[64];
     msg.getString(1, buf, sizeof(buf));
     display(buf, 0, 1, false);
   }
+
+} 
+
+
+// void handleLED(OSCMessage &msg, bool onOrOff) {   // this functions displays the words for the word maze
+//   if (msg.isInt(0)) { // check if the message is for displaying the first word
+//     int addressedLed = msg.getInt(0);
+//     Serial.println("LED " + String(addressedLed) + (onOrOff ? " ON" : " OFF"));
+//     LEDmanager(logicalLEDNumberToPhysicalPosition[addressedLed], onOrOff ? CRGB::Red : CRGB::Black);
+//   }
+
+// }
+void handleLED(OSCMessage &msg) {   // this functions displays the words for the word maze
+
+  // we recieve 8 booleans corrsponding to each light. loop over and light up if true
+  for(int i = 0; i < 7; i++) {
+    if (msg.getBoolean(i)) {
+      LEDmanager(logicalLEDNumberToPhysicalPosition[i], CRGB::Red);
+    }
+  }
+
+}
+
+void LEDmanager(int LEDnNumber, CRGB color){
+  // Set the color of the specified LED
+  leds[LEDnNumber] = color;
+  FastLED.show();
 
 }
